b6fabd0f8da6845aff57e15141079f7ee7869d7e
mario-lafuente <info@mariolafuente.com>
2021-03-22 16:10:37 -0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*--------------------------------------------------------------
+Selects elements 
+--------------------------------------------------------------*/
+const preloader = document.querySelector('.preloader');
+
+/*--------------------------------------------------------------
+Remove Loading
+--------------------------------------------------------------*/
+let stop = false;
+function loadingOff() {
+	//1 set opacity and later will remove the loader
+	if (!preloader.classList.contains('opacity') && !stop) {
+		preloader.classList.add('opacity');
+		setTimeout(loadingOff, 1000);
+	} else {
+		//2 remove or hide
+		preloader.classList.add('hide');
+	}
+}
+window.onload = () => {
+	setTimeout(loadingOff, 100);
+};
+
+/*--------------------------------------------------------------
+animate elements
+--------------------------------------------------------------*/
