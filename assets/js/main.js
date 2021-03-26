6d791e845dba7006c2157c0bbd88add66308b3b7
mario-lafuente <info@mariolafuente.com>
2021-03-26 16:33:48 -0600

    content working missing mobile if they want to

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,40 @@ const preloader = document.querySelector('.preloader');
 const sidebar = document.querySelector('.sidebar');
 const sidebarCollapse = document.querySelector('#collapseBar');
 
+const descTitle = document.querySelector('.descriptionBet');
+const descMobileMenuParent = document.querySelector('#dropdownBetMobile');
+const descMobileMenu = document.querySelector('.dropdownBetMobile');
+const descMobileItem = descMobileMenu.querySelectorAll('a');
+const descMobileContParent = document.querySelector('#pills-tabContent');
+const descMobileContChild = descMobileContParent.querySelectorAll('.tab-pane');
+
+//Cambie el panel
+const showContent = function (itemCl) {
+	descMobileContChild.forEach(item => {
+		item.classList.remove('show');
+		item.classList.remove('active');
+	});
+	const itemToChange = document.querySelector(`${itemCl}`);
+	itemToChange.classList.add('show');
+	itemToChange.classList.add('active');
+};
+
+//Cambie el panel al iniciar
+const descInit = function () {
+	descTitle.innerHTML = descMobileMenuParent.innerHTML;
+	//Cuando Selecciono algo en el menu cambie el titulo y cambien el panel
+	descMobileItem.forEach(item => {
+		if (item.className === 'dropdown-item') {
+			item.addEventListener('click', () => {
+				const name = item.innerHTML.toLowerCase().replace(' ', '');
+				showContent(`#pill-${name}`);
+				descMobileMenuParent.innerHTML = descTitle.innerHTML = item.innerHTML;
+			});
+		}
+	});
+};
+descInit();
+
 /*--------------------------------------------------------------
 Remove Loading
 --------------------------------------------------------------*/
